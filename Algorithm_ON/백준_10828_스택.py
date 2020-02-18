e57bf2aa36812a23d36aa84fcf4a3dc6d5ae82b1
jellymj <jellymj0114@gmail.com>
2020-02-19 08:59:29 +0900

    오늘도 화이팅

---
+++ Algorithm_ON/백준_10828_스택.py
@@ -0,0 +1,30 @@
+T = int(input())
+
+
+for tc in range(1, T+1):
+    n = int(input())
+    stack = []    
+    if n != 0 :
+        stack.append(n)
+    else:
+        stack.pop
+print(sum(stack))
+
+
+
+
+
+
+
+
+
+
+
+# for T in range(1,int(input())+1):
+#     a = int(input())
+#     stack = []
+#     if a !=0:
+#         stack.append(a)
+#     else:
+#         stack.pop
+# print(sum(stack))
---
+++ Algorithm_ON/백준_9012_괄호.py
@@ -0,0 +1,31 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    stack = []
+    result = "YES"
+    s = list(input())
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(s[i])
+        if s[i] == ')':
+            stack.pop
+            if len(stack) == i-len(stack)+1 
+
+            if len(stack) == 0:
+                result = "NO"
+                break
+            else:
+                stack.pop
+            #     if len(stack) != 0:
+            #         result =
+        if len(stack) != 0:
+
+            result = "NO"
+        else:
+            result = "Yes"
+            
+    print(result)
+
+
+
+      
